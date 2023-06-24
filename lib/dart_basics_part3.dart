46e8260863455963f1803c2057a5373a45a998ed
Doaa <abdelazezdoaa30@gmail.com>
2023-06-24 14:08:59 +0300

    ff4

--- lib/dart_basics_part3.dart
+++ lib/dart_basics_part3.dart
@@ -5,6 +5,7 @@ void main(){
 print(powerNum(5,4));
 print(powerNumnamed(num1: 5, num2: 4));
 }
+
 /*
 num powerNum(){
   var num=pow(5,4);
@@ -12,6 +13,7 @@ num powerNum(){
 }
 */
 // requierd
+//7okaa hates doaa
 
 num powerNum(num num1,num num2){
 
@@ -20,6 +22,11 @@ num powerNum(num num1,num num2){
 
 
 num powerNumnamed({required num num1,required num num2}){
+
+  return pow(num1, num2);
+}
+
+num poweNum(num num1,[num num2=0]){
 
   return pow(num1, num2);
 }
